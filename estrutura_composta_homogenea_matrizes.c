ea1ba99515b502a960d85b0a9eefce013a050e89
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-08-19 16:03:16 -0300

    Estrutura de matriz

---
+++ estrutura_composta_homogenea_matrizes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int matriz[2][3]={{1,2,3},{4,5,6}};
+    printf("Elemento da primeira linha e segunda coluna: %d\n", matriz[0][1]);
+
+    return 0;
+}
--- estrutura_composta_homogenea_vetor.c
+++ estrutura_composta_homogenea_vetor.c
@@ -3,7 +3,7 @@
 int main(){
     int numeros[5]={1,2,3,4,5}; //Array de inteiros
     char letras[3]={'A','B','C'}; //Array de caracteres
-    printf("Primeiro número: %d\n", numeros[2]);
+    printf("Primeiro número: %d\n", numeros[0]);
     printf("Primeira letra: %C\n", letras[0]);
 
     return 0;
