33112e283c179b892a53ea250f7b931390f01eb5
BeatrizParedes <beatrizparedes1999@gmail.com>
2025-08-19 16:00:05 -0300

    Estruturas homogêneas teste

---
+++ estrutura_composta_homogenea_vetor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int numeros[5]={1,2,3,4,5}; //Array de inteiros
+    char letras[3]={'A','B','C'}; //Array de caracteres
+    printf("Primeiro número: %d\n", numeros[2]);
+    printf("Primeira letra: %C\n", letras[0]);
+
+    return 0;
+}
